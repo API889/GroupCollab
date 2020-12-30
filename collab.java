5ac9b0f65a443006b255bd139e8ad512f65e89e6
VeenaVangala <veena.vangala@apisero.com>
2020-12-30 18:30:16 +0530

    Update collab.java

--- collab.java
+++ collab.java
@@ -1,6 +1,6 @@
 public class collab {
 	public static void main(String[] args) {
-		System.out.println("Hello this is Suhas");
+		System.out.println("Hello from local repo...GitHubDesktop");
 		System.out.println("Hello this is Veena");
 }
 }
